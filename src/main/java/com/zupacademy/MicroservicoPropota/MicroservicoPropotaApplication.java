050eb3506f7b24788fe67579fd2e6320237c2852
PedroMateus26 <pedromateusneto@gmail.com>
2021-04-22 12:05:38 -0300

    Projeto criado e container funcionando

---
+++ src/main/java/com/zupacademy/MicroservicoPropota/MicroservicoPropotaApplication.java
@@ -0,0 +1,13 @@
+package com.zupacademy.MicroservicoPropota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoPropotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoPropotaApplication.class, args);
+	}
+
+}
