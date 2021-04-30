19b614d2391de1cfa785bda849f3a27cc3a1d3a0
PedroMateus26 <pedromateusneto@gmail.com>
2021-04-30 20:50:08 -0300

    Login com keycloak ver 2

--- src/main/java/com/zupacademy/MicroservicoPropota/seguranca/WebSecurityConfig.java
+++ src/main/java/com/zupacademy/MicroservicoPropota/seguranca/WebSecurityConfig.java
@@ -1,25 +1,23 @@
 package com.zupacademy.MicroservicoPropota.seguranca;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(request->
+        http.csrf().disable().authorizeRequests(request ->
                 request.antMatchers("api/propostas/**")
+                        .hasAuthority("SCOPE_profile")
+                        .anyRequest()
                         .authenticated()
-        ).
-                oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 
 }
